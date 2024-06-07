4dbbe8482e6f57215793a4309077e321d30110bf
PIERINA BRITO <pierina.brito@tbiil.edu>
2024-06-06 20:56:45 -0500

    Easy Bank App

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <main className="flex min-h-screen w-full justify-between font-inter">
+          {children}
+          <div className="auth-asset">
+            <div>
+              <Image 
+                src="/icons/logo.svg"
+                alt="Auth image"
+                width={680}
+                height={680}
+              />
+
+            </div>
+          </div>
+      </main>
+    );
+  }
+  
